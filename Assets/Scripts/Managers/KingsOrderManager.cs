64e6410e2818d1be56a214a45da6a29f78dfabd8
stejwm <steve.jwm@gmail.com>
2025-03-01 00:03:35 -0500

    more ability icons

--- Assets/Scripts/Managers/KingsOrderManager.cs
+++ Assets/Scripts/Managers/KingsOrderManager.cs
@@ -8,10 +8,11 @@ using UnityEngine;
 public class KingsOrderManager : MonoBehaviour
 {
     public KingsOrder order;
-
     public TMP_Text title;
     public TMP_Text effect;
     public static KingsOrderManager _instance;
+
+    public GameObject parent;
 
 
     void Awake()
@@ -28,19 +29,57 @@ public class KingsOrderManager : MonoBehaviour
     public void Setup(){
         if (Game._instance.hero.orders.Count>0)
         {
-            this.gameObject.SetActive(true);
+            parent.SetActive(true);
             order = Game._instance.hero.orders[0];
-            title.text= order.Name;
-            effect.text= order.Description;
+            UpdateCardUI();
         }
         else{
-            this.gameObject.SetActive(false);
+            parent.SetActive(false);
         }
         
     }
 
     void OnMouseDown(){
-        Debug.Log("Order Used");
+        int index = Game._instance.hero.orders.IndexOf(order);
+
+        Game._instance.hero.orders.Remove(order);
+        
         StartCoroutine(order.Use());
+        if (index==0){
+            parent.SetActive(false);
+            order=null;
+        }
+        else{
+            order= Game._instance.hero.orders[0];
+        }
+        UpdateCardUI();
+        
+    }
+
+    public void UpdateCardUI(){
+        title.text= order.Name;
+        effect.text= order.Description;
+    }
+
+    public void CardLeft(){
+        int index = Game._instance.hero.orders.IndexOf(order);
+        if (index>0){
+            index--;
+            order=Game._instance.hero.orders[index];
+            UpdateCardUI();
+        }else{
+            this.GetComponent<MMSpringPosition>().BumpRandom();
+        }
+    }
+    public void CardRight(){
+        int index = Game._instance.hero.orders.IndexOf(order);
+        if (index<Game._instance.hero.orders.Count-1){
+            index++;
+            order=Game._instance.hero.orders[index];
+            UpdateCardUI();
+        }
+        else{
+            this.GetComponent<MMSpringPosition>().BumpRandom();
+        }
     }
 }
